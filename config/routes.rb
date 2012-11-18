64c14135596fdcd8e36289c417d48b5476a6cbfa
erikj <erik@erikj.me>
2012-11-17 18:05:00 -0700

    adding initial files for twitter-bootstrap demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 HerokuRails329Demo::Application.routes.draw do
+  resources :products
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
   match '/' => 'home#index'
